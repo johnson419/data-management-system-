73008e3df6b6e7c97d9deb223465f350a26c5ab1
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-07-06 23:13:36 +0300

    added functionality to display the indexed info

--- src/components/FileItem.js
+++ src/components/FileItem.js
@@ -6,6 +6,8 @@ const FileItem = ({ file }) => {
       <a href={file.url} target="_blank" rel="noopener noreferrer">
         {file.name}
       </a>
+      <p>Text Content: {file.textContent}</p>
+      <p>Keywords: {file.keywords.join(', ')}</p>
     </li>
   );
 };
